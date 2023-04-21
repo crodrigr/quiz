a0c5268b14d7d417ddba5a871916f4aacd577699
Camilo Rodriguez <crodrigr@gmail.com>
2023-04-20 19:57:12 -0500

    first commit

---
+++ src/main/java/com/uts/quiz/quiz/controllers/IndexController.java
@@ -0,0 +1,18 @@
+package com.uts.quiz.quiz.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class IndexController {
+
+    
+    @RequestMapping(value ="/index",   method = RequestMethod.GET)
+    public String index(Model model){
+        model.addAttribute("titulo", "Listado de clientes"); 
+        return "listar";
+    }
+    
+}
